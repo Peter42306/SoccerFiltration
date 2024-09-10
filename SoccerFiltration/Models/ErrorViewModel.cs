1b53d2086418570aef1958872893264ed5bf6515
Petr Zalizko <pzalizko@gmail.com>
2024-09-10 10:15:10 +0300

    Add project files.

---
+++ SoccerFiltration/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SoccerFiltration.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

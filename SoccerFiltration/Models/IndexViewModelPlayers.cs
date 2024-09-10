c9b918bf857983bc4c10c3a6006215e91160be95
Petr Zalizko <pzalizko@gmail.com>
2024-09-10 12:50:27 +0300

    App runs. Filtration works properly

---
+++ SoccerFiltration/Models/IndexViewModelPlayers.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace SoccerFiltration.Models
+{
+    public class IndexViewModelPlayers
+    {
+        public IEnumerable<Players> Players { get; set; } = new List<Players>();
+        public SelectList Teams { get; set; } = new SelectList(new List<Teams>(), "Id", "Name");
+        public string? Position { get; set; }
+    }
+}
---
+++ SoccerFiltration/Models/IndexViewModelTeams.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace SoccerFiltration.Models
+{
+    public class IndexViewModelTeams
+    {
+        public IEnumerable<Teams> Teams { get; set; } = new List<Teams>();
+        public string? Name {  get; set; }
+        public string? Coach { get; set; }
+    }
+}
